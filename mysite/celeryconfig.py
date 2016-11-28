0e8119c376175a1a263936e9ff9f4173c86f63c6
root <root@xozzslip.fvds.ru>
2016-11-28 16:29:40 -0500

    queue

--- mysite/celeryconfig.py
+++ mysite/celeryconfig.py
@@ -1,6 +1,12 @@
 from kombu import Exchange, Queue
 BROKER_URL = 'amqp://'
 
+CELERY_DEFAULT_QUEUE = 'thoughts'
+CELERY_QUEUES = (
+    Queue('thoughts', Exchange('thoughts'), routing_key='thoughts'),
+)
+
+
 CELERY_IGNORE_RESULT = True
 CELERY_TASK_RESULT_EXPIRES = 10000
 CELERY_ALWAYS_EAGER = False
